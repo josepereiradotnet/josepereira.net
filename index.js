ee06f8de19c81f950e2b8158132de6caf4a3ec42
josepereiradotnet <156430540+josepereiradotnet@users.noreply.github.com>
2024-11-15 12:02:30 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function redirectme() {
-    window.location.href = "https://www.linkedin.com/in/josepereiradotnet/";
+    //window.location.href = "https://www.linkedin.com/in/josepereiradotnet/";
     window.location.replace("https://www.linkedin.com/in/josepereiradotnet/");
 }
